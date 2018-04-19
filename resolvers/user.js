d018708b94248c6d6236f17494aac8d8eeea8fe9
Sameer Khan <sameer_khan@student.uml.edu>
2018-04-19 19:16:39 -0400

    Minor change

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,19 @@
+import bcrypt from 'bcrypt';
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...args }, { models }) => {
+      try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+        await models.User.create({ ...args, password: hashedPassword });
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
   },
 };
